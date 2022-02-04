8c2f24f98ebd93e7e5a5f6bb4074fdb7064fb881
alikabel <alikabel1992@gmail.com>
2022-02-04 14:52:53 +0200

    Complete Settings Screen.

---
+++ Settings/Cells/SignInCell.swift
@@ -0,0 +1,17 @@
+//
+//  SignInCell.swift
+//  Settings
+//
+//  Created by Mohamed Sayed on 04.02.22.
+//  Copyright © 2022 mohamed. All rights reserved.
+//
+
+import UIKit
+
+class SignInCell: UITableViewCell {
+    
+    @IBOutlet weak var signInImage: UIImageView!
+    @IBOutlet weak var signInLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    
+}
---
+++ Settings/Screens/SettingsScreen.swift
@@ -0,0 +1,142 @@
+//
+//  ViewController.swift
+//  settings
+//
+//  Created by Mohamed Sayed on 28.01.22.
+//  Copyright © 2022 mohamed. All rights reserved.
+//
+
+import UIKit
+
+class SettingsScreen: UIViewController {
+    
+    @IBOutlet weak var settingsTableView: UITableView!
+    
+    let signIn = ["Sign in to your iphone"]
+    
+    let general = ["General", "Accessibility", "Privacy"]
+    
+    let accounts = ["Passwords & Accounts"]
+    
+    let apps = ["Maps", "Safari", "News", "Health", "Shortcuts", "Siri & Search", "Photos", "Game Center"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
+        
+    }
+}
+
+extension SettingsScreen: UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return signIn.count
+        case 1:
+            return general.count
+        case 2:
+            return accounts.count
+        case 3:
+            return apps.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let settingsCell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath)
+
+        let signInCell = tableView.dequeueReusableCell(withIdentifier: "signInCell", for: indexPath) as! SignInCell
+
+        switch indexPath.section {
+        case 0:
+            signInCell.signInLabel.text = "Sign in to your iPhone"
+            signInCell.infoLabel.text = "Set up iCloud, the App store, and more. "
+            signInCell.signInImage.image = UIImage(named: "saiko_img")
+            signInCell.signInImage.layer.cornerRadius = signInCell.signInImage.frame.height / 2
+        case 1:
+            settingsCell.textLabel?.text = general[indexPath.row]
+        case 2:
+            settingsCell.textLabel?.text = accounts[indexPath.row]
+        case 3:
+            settingsCell.textLabel?.text = apps[indexPath.row]
+        default:
+            settingsCell.textLabel?.text = "Invalid"
+        }
+
+        if indexPath.section == 0 {
+            return signInCell
+        }else{
+            return settingsCell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        switch indexPath.section {
+        case 0:
+            print (signIn[indexPath.row])
+        case 1:
+            
+            switch indexPath.row {
+            case 0:
+                if let vc = storyboard?.instantiateViewController(withIdentifier: "GeneralScreen") as? GeneralScreen{
+                    navigationController?.pushViewController(vc, animated: true)
+                }
+                
+            case 1:
+                print (general[1])
+            case 2:
+                print (general[2])
+            default:
+                print ("Invalid")
+            }
+        case 2:
+            print (accounts[indexPath.row])
+        case 3:
+            switch indexPath.row {
+            case 0:
+                print (apps[0])
+            case 1:
+                print (apps[1])
+            case 2:
+                print (apps[2])
+            case 3:
+                print (apps[3])
+            case 4:
+                print (apps[4])
+            case 5:
+                print (apps[5])
+            case 6:
+                print (apps[6])
+            case 7:
+                print (apps[7])
+            default:
+                print ("Invalid")
+            }
+        default:
+            print ("Invalid")
+        }
+    
+//
+//        if indexPath.section == 1 && indexPath.row == 0 {
+//
+//            if let vc = storyboard?.instantiateViewController(identifier: "GeneralScreen") as? GeneralScreen{
+//                navigationController?.pushViewController(vc, animated: true)
+//
+//            }
+//        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
